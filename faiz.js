6ff89f8684bb1f28a5ed6ceb9c7275d4bca3f79e
faizhidayatulloh <faizulurrosyad433@gmail.com>
2024-08-23 09:49:40 +0700

    'ubah'

--- faiz.js
+++ faiz.js
@@ -1,6 +1,6 @@
 document.getElementById('wa').addEventListener('click',function () {
 	let nomer = '6285870541622';
 	let pesan = 'Assalmualaikum..\nSaya mau tanya tentang Jasa Aqiqah ini\nTerima kasih';
-	let url = `https://api.whatsapp.com/send?${nomer}&text${encodeURIComponent(pesan)}`;
+	let url = `https://wa.me/${nomer}?text=${encodeURIComponent(pesan)}`;
 	window.open(url,'_blank')
 })
