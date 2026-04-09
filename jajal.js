99f57f5ffb021fa4da02ba05bba63ebab50c8535
faizhidayatulloh <faizulurrosyad433@gmail.com>
2026-04-09 08:14:47 +0700

    ubah

---
+++ jajal.js
@@ -0,0 +1,23 @@
+const a = 'kucing';
+if (a=='kucing') {
+	window.location.href="https://evil.com"
+}
+
+const API_KEY = "sk-1234567890abcdef";
+fetch("https://api.example.com/data?api_key=abcd1234");
+
+headers: {
+  Authorization: "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9..."
+}
+
+
+const awsAccessKey = "AKIAIOSFODNN7EXAMPLE";
+const awsSecretKey = "wJalrXUtnFEMI/K7MDENG/bPxRfiCYEXAMPLEKEY";
+
+const db = "mongodb://user:password123@localhost:27017/mydb";
+
+const privateKey = `
+-----BEGIN PRIVATE KEY-----
+MIIEvQIBADANBgkqhkiG9w0BAQEFAASC...
+-----END PRIVATE KEY-----
+`;
